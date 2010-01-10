906b028061f09bdc533be27d031e2e8bb0f52ef0
andy <andy@1ffd1d58-2efc-45df-af60-190a33ec9863>
2010-01-10 21:14:30 +0000

    updating for debian build
    git-svn-id: https://yuma.svn.sourceforge.net/svnroot/yuma/trunk@615 1ffd1d58-2efc-45df-af60-190a33ec9863

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the yuma package
-#
-0 4	* * *	root	[ -x /usr/bin/yuma_maintenance ] && /usr/bin/yuma_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(yuma):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="yuma" command="/usr/bin/yuma"

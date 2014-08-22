dd057271209e40d4c6fb99689cfca6d125a19d2e
vladimir <vladimir@transpacket.com>
2014-08-22 17:27:49 +0200

    Extracted the API implementation code from yangrpc-example.c to yangrpc library and ubdated the build scripts

---
+++ netconf/src/yangrpc/yangrpc.h
@@ -0,0 +1,7 @@
+#include "val.h"
+
+typedef struct dummy {int dummy;} yangrpc_cb_t;
+
+yangrpc_cb_t* yangrpc_connect(char* host, char* user, char* pass);
+status_t yangrpc_exec(yangrpc_cb_t *server_cb, val_value_t* request_val, val_value_t** reply_val);
+void yangrpc_close(yangrpc_cb_t *yangrpc_cb);

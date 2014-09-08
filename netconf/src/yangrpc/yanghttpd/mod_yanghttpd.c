b084a3a42f235ef8027f204ed9636e6cef59721f
vladimir <vladimir@transpacket.com>
2014-09-08 12:46:21 +0200

    Completed the example of apache module providing access to a YANG database device using the yangrpc as simplified blocking transaction interface and yuma ncx API as data access/parser interface: * <hostname>/edit-config.html * <hostname>/state.xml * <hostname>/config.xml

--- netconf/src/yangrpc/yanghttpd/mod_yanghttpd.c
+++ netconf/src/yangrpc/yanghttpd/mod_yanghttpd.c
@@ -17,6 +17,7 @@
 #include "ncx.h"
 #include "val.h"
 #include "val_util.h"
+#include "val_parse.h"
 #include "yangrpc.h"
 
 yangrpc_cb_t* yangrpc_cb;
@@ -42,6 +43,12 @@ module AP_MODULE_DECLARE_DATA   yanghttpd_module =
 /* register_hooks: Adds a hook to the httpd process */
 static void register_hooks(apr_pool_t *pool) 
 {
+    int ret;
+    char username[512];
+    char private_key[512];
+    char public_key[512];
+    ret = getlogin_r(username, sizeof(username));
+    assert(ret==0);
     /* Hook the request handler */
     ap_hook_handler(example_handler, NULL, NULL, APR_HOOK_LAST);
     {
@@ -50,7 +57,9 @@ static void register_hooks(apr_pool_t *pool)
     	int argc = 1;
         res = yangrpc_init(argc, argv);
         assert(res==NO_ERR);
-        yangrpc_cb = yangrpc_connect("127.0.0.1"/*server*/,"root"/*user*/,"hadm1_123"/*password*/, "/home/vladimir/.ssh_/id_rsa.pub"/*public-key*/, "/home/vladimir/.ssh_/id_rsa"/*private-key*/);
+        sprintf(private_key, "%s%s", getenv("HOME"), "/.ssh/id_rsa");
+        sprintf(public_key, "%s%s", getenv("HOME"), "/.ssh/id_rsa.pub");
+        yangrpc_cb = yangrpc_connect("127.0.0.1"/*server*/,username/*user*/,""/*password*/, public_key, private_key);
     }
 }
 
@@ -73,7 +82,7 @@ boolean nodetest_fn (ncx_withdefaults_t withdef,
     return val_is_config_data(node);
 }
 
-void serialize_root(request_rec *r, val_value_t* root_val)
+void serialize_val(request_rec *r, val_value_t* root_val)
 {
     status_t res;
     FILE* fp;
@@ -91,7 +100,7 @@ void serialize_root(request_rec *r, val_value_t* root_val)
     ap_set_content_type(r, "application/xml;charset=utf-8");
 
 
-    if(0==strcmp(r->uri, "/yanghttpd/config.xml")) {
+    if(0==strcmp(r->uri, "/config.xml")) {
         testfn=nodetest_fn;
     } else {
         testfn=NULL;
@@ -101,7 +110,7 @@ void serialize_root(request_rec *r, val_value_t* root_val)
                         root_val,
                         &attrs,
                         TRUE/*docmode*/,
-                        TRUE/*xmlhdr*/,
+                        FALSE/*xmlhdr*/,
                         TRUE/*withns*/,
                         0/*startindent*/,
                         4/*indent*/,
@@ -165,6 +174,15 @@ static int read_post(request_rec *r, apr_table_t **tab)
         key = ap_getword(r->pool, &val, '=');
 
         ap_unescape_url((char*)key);
+        {
+            char *s;
+            for(s = val; *s; ++s) {
+                if ('+' == *s) {
+                    *s = ' ';
+                }
+            }
+        }
+
         ap_unescape_url((char*)val);
 
         apr_table_merge(*tab, key, val);
@@ -173,20 +191,109 @@ static int read_post(request_rec *r, apr_table_t **tab)
     return OK;
 }
 
+static int edit_config_form(request_rec *r)
+{
+    ap_rprintf(r, "%s","<html><body><form name=\"input\" action=\"/edit-config.xml\" method=\"post\"><textarea name=\"config\" rows=25 cols=80 ></textarea><input type=\"submit\" value=\"Submit\"></form></body></html>");
+    return OK;
+}
 
 static int edit_config(request_rec *r)
 {
+    status_t res;
+    ncx_module_t * ietf_netconf_mod;
     apr_table_t *tab=NULL;
-    const char* config_str;
+    const char* config_xml_str;
+    obj_template_t* root_obj;
+    val_value_t* root_val;
     int rc;
+    obj_template_t* edit_config_rpc_obj;
+    obj_template_t* input_obj;
+    obj_template_t* commit_rpc_obj;
+    val_value_t* edit_config_rpc_val;
+    val_value_t* edit_config_rpc_reply_val;
+    val_value_t* commit_rpc_val;
+    val_value_t* commit_rpc_reply_val;
+    char* rpc_format_str;
+    char* rpc_str;
+
+    FILE *fp = NULL;
     rc = read_post(r, &tab);
     if(rc!=OK) return rc;
 
-    config_str = apr_table_get(tab, "config") ;
-    if(config_str==NULL) {
+    config_xml_str = apr_table_get(tab, "config") ;
+    if(config_xml_str==NULL) {
         return DECLINED;
     }
-    ap_rprintf(r,"%s",config_str);
+    //ap_rprintf(r,"%s",config_xml_str);
+
+#if 0
+    rc = val_set_cplxval_obj(dst_val,dst_val->obj,config_xml_str);
+    if(rc != NO_ERR) {
+        return DECLINED;
+    }
+#else
+    fp = fmemopen((void*)config_xml_str, strlen(config_xml_str), "r");
+    res = ncxmod_load_module ("ietf-netconf", NULL, NULL, &ietf_netconf_mod);
+    root_obj = ncx_find_object(ietf_netconf_mod, "config");
+
+    rc = xml_rd_open_file (fp, root_obj, &root_val);
+
+
+    edit_config_rpc_obj = ncx_find_object(ietf_netconf_mod, "edit-config");
+    assert(obj_is_rpc(edit_config_rpc_obj));
+    input_obj = obj_find_child(edit_config_rpc_obj, NULL, "input");
+    assert(input_obj!=NULL);
+
+
+    rpc_format_str = "<input><target><candidate/></target>%s</input>";
+    rpc_str = malloc(strlen(rpc_format_str)+strlen(config_xml_str)+1);
+    sprintf(rpc_str, rpc_format_str, config_xml_str);
+
+    edit_config_rpc_val = val_new_value();
+    val_init_from_template(edit_config_rpc_val, edit_config_rpc_obj);
+    res = val_set_cplxval_obj(edit_config_rpc_val, input_obj, rpc_str);
+    free(rpc_str);
+    if(res != NO_ERR) {
+        val_free_value(edit_config_rpc_val);
+        edit_config_rpc_val = NULL;
+        edit_config_rpc_reply_val=NULL;
+    } else {
+        res = yangrpc_exec(yangrpc_cb, edit_config_rpc_val, &edit_config_rpc_reply_val);
+        assert(res==NO_ERR);
+    }
+
+    if(edit_config_rpc_reply_val!=NULL && NULL!=val_find_child(edit_config_rpc_reply_val,NULL,"ok")) {
+        commit_rpc_obj = ncx_find_object(ietf_netconf_mod, "commit");
+        assert(obj_is_rpc(commit_rpc_obj));
+        commit_rpc_val = val_new_value();
+        val_init_from_template(commit_rpc_val, commit_rpc_obj);
+        res = yangrpc_exec(yangrpc_cb, commit_rpc_val, &commit_rpc_reply_val);
+        assert(res==NO_ERR);
+    } else {
+        commit_rpc_val=NULL;
+        commit_rpc_reply_val=NULL;
+    }
+
+
+#endif
+    ap_rprintf(r,"<netconf-chat>");
+    if(edit_config_rpc_val) {
+        serialize_val(r, edit_config_rpc_val);
+        val_free_value(edit_config_rpc_val);
+    }
+    if(edit_config_rpc_reply_val) {
+        serialize_val(r, edit_config_rpc_reply_val);
+        val_free_value(edit_config_rpc_reply_val);
+    }
+    if(commit_rpc_val) {
+        serialize_val(r, commit_rpc_val);
+        val_free_value(commit_rpc_val);
+    }
+    if(commit_rpc_reply_val) {
+        serialize_val(r, commit_rpc_reply_val);
+        val_free_value(commit_rpc_reply_val);
+    }
+    ap_rprintf(r,"</netconf-chat>");
     apr_table_clear(tab);
     return OK;
 }
@@ -212,15 +319,18 @@ static int example_handler(request_rec *r)
      * If it is, we accept it and do our things, it not, we simply return DECLINED,
      * and Apache will try somewhere else.
      */
-    if (!r->handler || (0!=strcmp(r->uri, "/yanghttpd/config.xml") &&
-                        0!=strcmp(r->uri, "/yanghttpd/state.xml") &&
-                        0!=strcmp(r->uri, "/yanghttpd/edit-config.html")
+    if (!r->handler || (0!=strcmp(r->uri, "/config.xml") &&
+                        0!=strcmp(r->uri, "/state.xml") &&
+                        0!=strcmp(r->uri, "/edit-config.html") &&
+                        0!=strcmp(r->uri, "/edit-config.xml")
        )) return (DECLINED);
 
     
     res = ncxmod_load_module ("ietf-netconf", NULL, NULL, &ietf_netconf_mod);
     assert(res==NO_ERR);
-    if(0==strcmp(r->uri, "/yanghttpd/edit-config.html")) {
+    if(0==strcmp(r->uri, "/edit-config.html")) {
+        return edit_config_form(r);
+    } else if(0==strcmp(r->uri, "/edit-config.xml")) {
         return edit_config(r);
     }
 
@@ -247,15 +357,23 @@ static int example_handler(request_rec *r)
     assert(res==NO_ERR);
 
     {
+        obj_template_t* config_obj;
+        val_value_t* config_val;
     	val_value_t* data_val;
     	val_value_t* interfaces_val;
     	val_value_t* interface_val;
     	char* interface_row_str[512];
 
         data_val = val_find_child(reply_val,NULL,"data");
-        serialize_root(r, data_val);
+        config_obj = ncx_find_object(ietf_netconf_mod, "config");
+        config_val = val_new_value();
+        val_init_from_template(config_val, config_obj);
+        val_move_children(data_val,config_val);
+        serialize_val(r, config_val);
+        val_free_value(config_val);
 
     }
+    val_free_value(request_val);
     val_free_value(reply_val);
     return OK;
 }

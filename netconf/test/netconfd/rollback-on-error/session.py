e87774e6efc36875e35a84052a71e6bee8c180cf
Vladimir Vassilev <vladimir@transpacket.com>
2016-09-23 18:11:39 +0200

    Added assert calls checking rpc-reply for expected ok or rpc-error data.

--- netconf/test/netconfd/rollback-on-error/session.py
+++ netconf/test/netconfd/rollback-on-error/session.py
@@ -13,15 +13,20 @@ if(conn==None):
 yangcli(conn, "replace /system/hostname value='ok3'")
 yangcli(conn, "replace /system/location value='ok4'")
 result=yangcli(conn, "commit")
+assert(result.xpath('./ok'))
 
 yangcli(conn, "replace /system/hostname value='error1'")
 yangcli(conn, "replace /system/location value='ok5'")
 result=yangcli(conn, "commit")
+assert(result.xpath('./rpc-error'))
 yangcli(conn, "discard-changes")
+
 
 yangcli(conn, "replace /system/hostname value='ok6'")
 yangcli(conn, "replace /system/location value='error2'")
 result=yangcli(conn, "commit")
+assert(result.xpath('./rpc-error'))
 yangcli(conn, "discard-changes")
+
 
 print("Done.")

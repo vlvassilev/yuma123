1d52f35e3f60c15234ba86c48609302f7feb5e53
Vladimir Vassilev <vladimir@transpacket.com>
2016-11-10 09:47:01 +0100

    Added empty edit callback for /alarms.

--- example-modules/ietf-alarms/ietf-alarms.c
+++ example-modules/ietf-alarms/ietf-alarms.c
@@ -21,6 +21,69 @@
 #include <string.h>
 #include <assert.h>
 
+static status_t
+    y_ietf_alarms_alarms_edit (
+        ses_cb_t *scb,
+        rpc_msg_t *msg,
+        agt_cbtyp_t cbtyp,
+        op_editop_t editop,
+        val_value_t *newval,
+        val_value_t *curval)
+{
+    status_t res;
+    val_value_t *errorval;
+    const xmlChar *errorstr;
+
+    res = NO_ERR;
+    errorval = NULL;
+    errorstr = NULL;
+
+    switch (cbtyp) {
+    case AGT_CB_VALIDATE:
+        /* description-stmt validation here */
+        break;
+    case AGT_CB_APPLY:
+        /* database manipulation done here */
+        break;
+    case AGT_CB_COMMIT:
+        /* device instrumentation done here */
+        switch (editop) {
+        case OP_EDITOP_LOAD:
+        case OP_EDITOP_MERGE:
+        case OP_EDITOP_REPLACE:
+        case OP_EDITOP_CREATE:
+        case OP_EDITOP_DELETE:
+            /* device instrumentation here */
+            break;
+        default:
+            assert(0);
+        }
+
+        break;
+    case AGT_CB_ROLLBACK:
+        /* undo device instrumentation here */
+        break;
+    default:
+        res = SET_ERROR(ERR_INTERNAL_VAL);
+    }
+
+    /* if error: set the res, errorstr, and errorval parms */
+    if (res != NO_ERR) {
+        agt_record_error(
+            scb,
+            &msg->mhdr,
+            NCX_LAYER_CONTENT,
+            res,
+            NULL,
+            NCX_NT_STRING,
+            errorstr,
+            NCX_NT_VAL,
+            errorval);
+    }
+
+    return res;
+}
+
 /* The 3 mandatory callback functions: y_ietf_alarms_init, y_ietf_alarms_init2, y_ietf_alarms_cleanup */
 
 status_t

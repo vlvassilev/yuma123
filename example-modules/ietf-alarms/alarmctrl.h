0bc7da11544f2b21a0655e8e83178b2e37132a3a
Vladimir Vassilev <vladimir@transpacket.com>
2016-11-13 06:43:14 +0100

    Removed unused defs.

--- example-modules/ietf-alarms/alarmctrl.h
+++ example-modules/ietf-alarms/alarmctrl.h
@@ -1,2 +1,1 @@
-void alarm_event_w_type(char* description_str, char* severity, char* type, int enable);
 int alarmctrl_event(char* resource_str, char* alarm_type_id_str, char* alarm_type_qualifier_str, char* severity_str, char* alarm_text_str, int enable);

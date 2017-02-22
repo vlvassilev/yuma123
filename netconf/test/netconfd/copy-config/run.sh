241dd6b22d59bd77d913da13c7a90e34cd52e977
Vladimir Vassilev <vladimir@transpacket.com>
2017-02-22 16:45:36 +0100

    Fixed run.sh failing if tmp directory already exists or /tmp/ncxserver.sock does not exist.

--- netconf/test/netconfd/copy-config/run.sh
+++ netconf/test/netconfd/copy-config/run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
-rm -rf tmp
+rm -rf tmp || true
 mkdir tmp
 cp startup-cfg.xml tmp
-rm /tmp/ncxserver.sock ; /usr/sbin/netconfd --module=ietf-interfaces --module=iana-if-type --startup=tmp/startup-cfg.xml --superuser=$USER 1>tmp/netconfd.stdout 2>tmp/netconfd.stderr &
+rm /tmp/ncxserver.sock || true
+/usr/sbin/netconfd --module=ietf-interfaces --module=iana-if-type --startup=tmp/startup-cfg.xml --superuser=$USER 1>tmp/netconfd.stdout 2>tmp/netconfd.stderr &
 
 NETCONFD_PID=$!
 sleep 3
--- netconf/test/netconfd/multiple-edit-callbacks/run.sh
+++ netconf/test/netconfd/multiple-edit-callbacks/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
-rm -rf tmp
+rm -rf tmp || true
 mkdir tmp
 cp startup-cfg.xml tmp
+rm /tmp/ncxserver.sock || true
 /usr/sbin/netconfd --module=test-multiple-edit-callbacks-a --module=test-multiple-edit-callbacks-b --module=iana-if-type --startup=tmp/startup-cfg.xml --superuser=$USER 1>tmp/netconfd.stdout 2>tmp/netconfd.stderr &
 NETCONFD_PID=$!
 sleep 1

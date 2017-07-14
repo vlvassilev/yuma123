83338dede7264b9674c121b82181e6a8c96a32c2
Vladimir Vassilev <vladimir@transpacket.com>
2017-07-14 08:58:30 +0200

    Added testcase for xpath-deref.

---
+++ netconf/test/netconfd/xpath-deref/run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+rm -rf tmp || true
+mkdir tmp
+if [ "$RUN_WITH_CONFD" != "" ] ; then
+  cp *.yang tmp
+  cd tmp
+  killall -KILL confd || true
+  echo "Starting confd: $RUN_WITH_CONFD"
+  source $RUN_WITH_CONFD/confdrc
+  confdc -c /usr/share/yuma/modules/ietf/ietf-interfaces.yang --yangpath /usr/share/yuma/modules/ietf -o ietf-interfaces.fxs
+  confdc -c /usr/share/yuma/modules/ietf/iana-if-type.yang --yangpath /usr/share/yuma/modules/ietf -o iana-if-type.fxs
+  confdc -c ../test-xpath-deref.yang --yangpath /usr/share/yuma/modules/ietf -o test-xpath-deref.fxs
+  NCPORT=2022
+  NCUSER=admin
+  NCPASSWORD=admin
+  confd --verbose --foreground --addloadpath ${RUN_WITH_CONFD}/src/confd --addloadpath ${RUN_WITH_CONFD}/src/confd --addloadpath ${RUN_WITH_CONFD}/src/confd/yang --addloadpath ${RUN_WITH_CONFD}/src/confd/aaa --addloadpath . 2>&1 1>server.log &
+  SERVER_PID=$!
+  cd ..
+else
+  killall -KILL netconfd || true
+  rm /tmp/ncxserver.sock || true
+  /usr/sbin/netconfd --module=./test-xpath-deref.yang --no-startup --superuser=$USER 2>&1 1>tmp/server.log &
+  SERVER_PID=$!
+fi
+sleep 3
+python session.litenc.py --server=$NCSERVER --port=$NCPORT --user=$NCUSER --password=$NCPASSWORD
+kill -KILL $SERVER_PID
+cat tmp/server.log
+sleep 1

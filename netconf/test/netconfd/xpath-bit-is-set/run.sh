9ec583b86ef7f0359a485eade9ba65be3db50806
Vladimir Vassilev <vladimir@transpacket.com>
2017-07-26 14:50:14 +0200

    Added broken model to test detection of invalid bit names in bit-is-set xpath expressions.

--- netconf/test/netconfd/xpath-bit-is-set/run.sh
+++ netconf/test/netconfd/xpath-bit-is-set/run.sh
@@ -6,7 +6,7 @@ function expected_fail {
     #echo "GOOD ERROR"
     return 0
   else
-    #echo "BAD OK"
+    echo "ERROR: Expected FAIL returned OK."
     return -1
   fi
 }
@@ -22,18 +22,23 @@ if [ "$RUN_WITH_CONFD" != "" ] ; then
   confdc -c /usr/share/yuma/modules/ietf/ietf-interfaces.yang --yangpath /usr/share/yuma/modules/ietf -o ietf-interfaces.fxs
   confdc -c /usr/share/yuma/modules/ietf/iana-if-type.yang --yangpath /usr/share/yuma/modules/ietf -o iana-if-type.fxs
   confdc -c test-xpath-bit-is-set.yang --yangpath /usr/share/yuma/modules/ietf -o test-xpath-bit-is-set.fxs
+  confdc -c test-xpath-bit-is-set-bad-bit-name.yang --yangpath /usr/share/yuma/modules/ietf -o test-xpath-bit-is-set-bad-bit-name.fxs
   NCPORT=2022
   NCUSER=admin
   NCPASSWORD=admin
   confd --verbose --foreground --addloadpath ${RUN_WITH_CONFD}/src/confd --addloadpath ${RUN_WITH_CONFD}/src/confd --addloadpath ${RUN_WITH_CONFD}/src/confd/yang --addloadpath ${RUN_WITH_CONFD}/src/confd/aaa --addloadpath . 2>&1 1>server.log &
   SERVER_PID=$!
   sleep 3
+  echo "Loading good.xml ..."
   confd_load -l ../good.xml
+  echo "Loading bad.xml"
   expected_fail "confd_load -l ../bad.xml"
   cd ..
 else
   killall -KILL netconfd || true
   rm /tmp/ncxserver.sock || true
-  /usr/sbin/netconfd --module=./test-xpath-bit-is-set.yang --validate-config-only --startup=good.xml
-  expected_fail "/usr/sbin/netconfd --module=./test-xpath-bit-is-set.yang --validate-config-only --startup=bad.xml"
+  /usr/sbin/netconfd --module=./test-xpath-bit-is-set.yang --validate-config-only --no-startup
+  expected_fail "/usr/sbin/netconfd --module=./test-xpath-bit-is-set-bad-bit-name.yang --validate-config-only --no-startup"
+  /usr/sbin/netconfd --module=/usr/share/yuma/modules/ietf/iana-if-type.yang --module=./test-xpath-bit-is-set.yang --validate-config-only --startup=good.xml
+  expected_fail "/usr/sbin/netconfd --module=/usr/share/yuma/modules/ietf/iana-if-type.yang --module=./test-xpath-bit-is-set.yang --validate-config-only --startup=bad.xml"
 fi

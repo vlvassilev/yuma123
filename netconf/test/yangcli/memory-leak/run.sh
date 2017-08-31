4637f78f8d69b5c1440d1133045a27ab98bf2ac8
Vladimir Vassilev <vladimir@transpacket.com>
2017-08-31 19:06:33 +0200

    Added testcase for detecting memory leaks.

---
+++ netconf/test/yangcli/memory-leak/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+rm -rf tmp || true
+mkdir tmp
+if [ "$RUN_WITH_CONFD" != "" ] ; then
+  cp *.yang tmp
+  cd tmp
+  killall -KILL confd || true
+  echo "Starting confd: $RUN_WITH_CONFD"
+  source $RUN_WITH_CONFD/confdrc
+  confdc -c /usr/share/yuma/modules/ietf/ietf-interfaces.yang --yangpath /usr/share/yuma/modules/ietf -o ietf-interfaces.fxs
+  confdc -c /usr/share/yuma/modules/ietf/iana-if-type.yang --yangpath /usr/share/yuma/modules/ietf -o iana_if_type.fxs
+  export NCSERVER=localhost
+  export NCPORT=2022
+  export NCUSER=admin
+  export NCPASSWORD=admin
+  confd --verbose --foreground --addloadpath ${RUN_WITH_CONFD}/src/confd --addloadpath ${RUN_WITH_CONFD}/src/confd --addloadpath ${RUN_WITH_CONFD}/src/confd/yang --addloadpath ${RUN_WITH_CONFD}/src/confd/aaa --addloadpath . 2>&1 1>server.log &
+  SERVER_PID=$!
+  cd ..
+else
+  killall -KILL netconfd || true
+  export NCSERVER=localhost
+  export NCPORT=830
+  export NCUSER=${USER}
+  export NCPASSWORD=""
+  rm /tmp/ncxserver.sock || true
+  /usr/sbin/netconfd --module=/usr/share/yuma/modules/ietf/ietf-interfaces.yang --module=/usr/share/yuma/modules/ietf/iana-if-type.yang  --no-startup --superuser=$USER 2>&1 1>tmp/server.log &
+  SERVER_PID=$!
+fi
+sleep 3
+expect session.start-quit.exp
+expect session.connect-quit.exp
+kill -KILL $SERVER_PID
+cat tmp/server.log
+sleep 1

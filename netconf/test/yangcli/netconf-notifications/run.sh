11a417f28339a5066c49d07a36673401c7e6651e
Vladimir Vassilev <vladimir@transpacket.com>
2017-10-06 13:00:08 +0200

    Added yangcli notification subscription test case.

---
+++ netconf/test/yangcli/netconf-notifications/run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+rm -rf tmp || true
+mkdir tmp
+if [ "$RUN_WITH_CONFD" != "" ] ; then
+  cd tmp
+  killall -KILL confd || true
+  echo "Starting confd: $RUN_WITH_CONFD"
+  source $RUN_WITH_CONFD/confdrc
+  confdc -c /usr/share/yuma/modules/ietf/ietf-interfaces.yang --yangpath /usr/share/yuma/modules/ietf -o ietf-interfaces.fxs
+  confdc -c /usr/share/yuma/modules/ietf/iana-if-type.yang --yangpath /usr/share/yuma/modules/ietf -o iana_if_type.fxs
+  export NCSERVER=localhost
+  export NCPORT=2022
+  export NCUSER=admin
+  export NCPASSWORD=admin
+  confd --verbose --foreground --addloadpath ${RUN_WITH_CONFD}/src/confd --addloadpath ${RUN_WITH_CONFD}/src/confd --addloadpath ${RUN_WITH_CONFD}/src/confd/yang --addloadpath ${RUN_WITH_CONFD}/src/confd/aaa --addloadpath . 2>&1 1>server.log &
+  SERVER_PID=$!
+  cd ..
+else
+  killall -KILL netconfd || true
+  rm /tmp/ncxserver.sock || true
+  export NCSERVER=localhost
+  export NCPORT=830
+  export NCUSER=${USER}
+  export NCPASSWORD=
+  /usr/sbin/netconfd --module=/usr/share/yuma/modules/ietf/ietf-interfaces.yang --module=/usr/share/yuma/modules/ietf/iana-if-type.yang --no-startup --superuser=$USER 2>&1 1>tmp/server.log &
+  SERVER_PID=$!
+fi
+
+sleep 3
+expect session.exp
+#kill $SERVER_PID
+cat tmp/server.log
+sleep 1

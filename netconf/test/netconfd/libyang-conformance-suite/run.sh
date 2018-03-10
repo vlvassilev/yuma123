9c1c4e5414006c35472e96af5bd6134e20298a29
Vladimir Vassilev <vladimir@transpacket.com>
2018-03-11 00:15:43 +0100

    Added loop and 2 of the 60 conformance directories.

--- netconf/test/netconfd/libyang-conformance-suite/run.sh
+++ netconf/test/netconfd/libyang-conformance-suite/run.sh
@@ -9,30 +9,64 @@ rm -rf tmp || true
 mkdir tmp
 
 LIB_YANG_PATH=../../../../../libyang
-MODULE=sec7_19_1/mod1.yang
-FAIL=0
-OK=0
+test()
+{
+    declare -a schema_load_fail=("${!2}")
+    declare -a data_file_load_fail=("${!3}")
 
-EXPECTED="OK"
-/usr/sbin/netconfd --validate-config-only --startup-error=stop --no-startup --module= ${LIB_YANG_PATH}/tests/conformance/${MODULE} 1>tmp/netconfd.stdout 2>tmp/netconfd.stderr
-RES=$?
-cat tmp/netconfd.stdout
-if [ "$RES" != "0" ] ; then
-  if [ "$EXPECTED" == "OK" ] ; then
-    echo "FAIL: ${MODULE}"
-    FAIL=$(($FAIL+1))
-  else
-    echo "OK:  ${MODULE}"
-    OK=$(($OK+1))
-  fi
-else
-  if [ "$EXPECTED" == "OK" ] ; then
-    echo "FAIL: ${MODULE}"
-    FAIL=$(($FAIL+1))
-  else
-    echo "OK: ${MODULE}"
-    OK=$(($OK+1))
-  fi
-fi
-echo "TOTAL: OKs=$OK FAILs=$FAIL"
-exit $FAIL
+    FAIL=0
+    OK=0
+
+    for index in ${!schema_load_fail[*]}
+    do
+        #printf "%4d: %s\n" $index ${schema_load_fail[$index]}
+        if [ ${schema_load_fail[$index]} == "1" ] ; then
+            EXPECTED="OK"
+        else
+            EXPECTED="FAIL"
+        fi
+        MODULE=${1}/mod$(($index+1)).yang
+        #echo "Testing EXPECTED=$EXPECTED $MODULE ..."
+        /usr/sbin/netconfd --validate-config-only --startup-error=stop --no-startup --module= ${LIB_YANG_PATH}/tests/conformance/${1}/mod$(($index+1)).yang 1>tmp/${1}_mod$(($index+1)).yang.stdout 2>tmp/${1}_mod$(($index+1)).yang.stderr
+        RES=$?
+        #echo "RES="$RES
+        if [ "$RES" != "0" ] ; then
+            if [ "$EXPECTED" == "OK" ] ; then
+                echo "FAIL: ${MODULE}"
+                FAIL=$(($FAIL+1))
+            else
+                echo "OK:   ${MODULE}"
+                OK=$(($OK+1))
+            fi
+        else
+            if [ "$EXPECTED" != "OK" ] ; then
+                echo "FAIL: ${MODULE}"
+                FAIL=$(($FAIL+1))
+            else
+                echo "OK:   ${MODULE}"
+                OK=$(($OK+1))
+            fi
+        fi
+    done
+    echo "TOTAL: OKs=$OK FAILs=$FAIL"
+    return $FAIL
+}
+TOTAL_FAILS=0
+
+TEST_DIR="sec5_1"
+TEST_SCHEMA_COUNT=6
+TEST_SCHEMA_LOAD_FAIL=(1 1 1 1 1 0)
+TEST_DATA_FILE_COUNT=0
+TEST_DATA_FILE_LOAD_FAIL=()
+test $TEST_DIR TEST_SCHEMA_LOAD_FAIL[@] TEST_DATA_FILE_LOAD_FAIL[@]
+TOTAL_FAILS=$(($TOTAL_FAILS+$?))
+
+TEST_DIR=sec7_19_1
+TEST_SCHEMA_COUNT=15
+TEST_SCHEMA_LOAD_FAIL=(1 1 1 0 0 0 0 0 0 0 0 0 0 0 0)
+TEST_DATA_FILE_COUNT=0
+TEST_DATA_FILE_LOAD_FAIL=()
+test $TEST_DIR TEST_SCHEMA_LOAD_FAIL[@] TEST_DATA_FILE_LOAD_FAIL[@]
+TOTAL_FAILS=$(($TOTAL_FAILS+$?))
+
+exit $TOTAL_FAILS

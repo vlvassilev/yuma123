9296d2b49c9c9dfce3a6e4ab189f0cc258b587cf
Vladimir Vassilev <vladimir@transpacket.com>
2018-03-18 13:02:18 +0100

    Started 2 instances of netconfd and yangcli batch-mode command connecting to the opendaylight northbound NETCONF interface.

--- netconf/test/netconfd/opendaylight/run.sh
+++ netconf/test/netconfd/opendaylight/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
-cd
+
+cd tmp
 wget https://nexus.opendaylight.org/content/groups/public/org/opendaylight/integration/distribution-karaf/0.6.3-Carbon/distribution-karaf-0.6.3-Carbon.tar.gz
 tar -xzvf distribution-karaf-0.6.3-Carbon.tar.gz
 cd distribution-karaf-0.6.3-Carbon/
@@ -8,3 +9,20 @@ cd distribution-karaf-0.6.3-Carbon/
 ./bin/client -u karaf feature:install odl-netconf-all odl-netconf-ssh odl-netconf-connector-all
 #install GUI support
 ./bin/client -u karaf feature:install odl-dlux-core odl-dluxapps-applications odl-dluxapps-nodes odl-dluxapps-yangman odl-dluxapps-yangui odl-dluxapps-yangutils odl-dluxapps-yangvisualizer
+cd ../../
+
+#start netconfd nodes - avoid 1830 used by the opendaylight NETCONF server.
+killall -KILL netconfd || true
+NCPORT0=2830
+NCPORT1=3830
+rm /tmp/ncxserver.${NCPORT0}.sock || true
+/usr/sbin/netconfd --modpath=.:/usr/share/yuma/modules/ --module=iana-if-type --module=ietf-interfaces --no-startup --superuser=$USER --ncxserver-sockname=/tmp/ncxserver.${NCPORT0}.sock --port=${NCPORT0} &
+SERVER0_PID=$!
+rm /tmp/ncxserver.${NCPORT1}.sock || true
+/usr/sbin/netconfd --modpath=.:/usr/share/yuma/modules/ --module=iana-if-type --module=ietf-interfaces --no-startup --superuser=$USER --ncxserver-sockname=/tmp/ncxserver.${NCPORT1}.sock --port=${NCPORT1} &
+SERVER1_PID=$!
+
+#configure topology - TODO
+yangcli --server=localhost --user=admin --ncport=1830 --run-command="xget /" --batch-mode --password=admin
+
+#test - TODO

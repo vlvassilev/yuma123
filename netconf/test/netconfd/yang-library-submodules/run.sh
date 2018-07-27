c7519f49932486b72e09a760a245746134a30f28
Vladimir Vassilev <vladimir@transpacket.com>
2018-07-27 15:56:17 +0200

    Added testcase validating yang-library correctly handles submodules.

---
+++ netconf/test/netconfd/yang-library-submodules/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+rm -rf tmp || true
+mkdir tmp
+if [ "$RUN_WITH_CONFD" != "" ] ; then
+  cp *.yang tmp
+  cd tmp
+  killall -KILL confd || true
+  echo "Starting confd: $RUN_WITH_CONFD"
+  source $RUN_WITH_CONFD/confdrc
+  confdc -c test-yang-library-submodules.yang --yangpath . -o test-yang-library.fxs
+  NCPORT=2022
+  NCUSER=admin
+  NCPASSWORD=admin
+  confd --verbose --foreground --addloadpath ${RUN_WITH_CONFD}/src/confd --addloadpath ${RUN_WITH_CONFD}/src/confd --addloadpath ${RUN_WITH_CONFD}/src/confd/yang --addloadpath ${RUN_WITH_CONFD}/src/confd/aaa --addloadpath . 2>&1 1>server.log &
+  SERVER_PID=$!
+  cd ..
+else
+  killall -KILL netconfd || true
+  rm /tmp/ncxserver.sock || true
+  /usr/sbin/netconfd --module=./test-yang-library-submodules.yang --no-startup --superuser=$USER 2>&1 1>tmp/server.log &
+  SERVER_PID=$!
+fi
+sleep 3
+python session.litenc.py --server=$NCSERVER --port=$NCPORT --user=$NCUSER --password=$NCPASSWORD
+#python session.yangcli.py --server=$NCSERVER --port=$NCPORT --user=$NCUSER --password=$NCPASSWORD
+kill -KILL $SERVER_PID
+cat tmp/server.log
+sleep 1
---
+++ netconf/test/netconfd/yang-library-submodules/session.litenc.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+import time
+import sys, os
+sys.path.append("../../litenc")
+import litenc
+import litenc_lxml
+import lxml
+import argparse
+
+def main():
+	print("""
+#Description: Demonstrate that ietf-yang-library is implemented.
+#Procedure:
+#1 - Verify /modules-state/module[name='test-yang-library-submodules'] container is present.
+""")
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--server", help="server name e.g. 127.0.0.1 or server.com (127.0.0.1 if not specified)")
+	parser.add_argument("--user", help="username e.g. admin ($USER if not specified)")
+	parser.add_argument("--port", help="port e.g. 830 (830 if not specified)")
+	parser.add_argument("--password", help="password e.g. mypass123 (passwordless if not specified)")
+
+	args = parser.parse_args()
+
+	if(args.server==None or args.server==""):
+		server="127.0.0.1"
+	else:
+		server=args.server
+
+	if(args.port==None or args.port==""):
+		port=830
+	else:
+		port=int(args.port)
+
+	if(args.user==None or args.user==""):
+		user=os.getenv('USER')
+	else:
+		user=args.user
+
+	if(args.password==None or args.password==""):
+		password=None
+	else:
+		password=args.password
+
+
+	conn_raw = litenc.litenc()
+	ret = conn_raw.connect(server=server, port=port, user=user, password=password)
+	if ret != 0:
+		print "[FAILED] Connecting to server=%(server)s:" % {'server':server}
+		return(-1)
+	print "[OK] Connecting to server=%(server)s:" % {'server':server}
+	conn=litenc_lxml.litenc_lxml(conn_raw)
+	ret = conn_raw.send("""
+<hello xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+ <capabilities>
+  <capability>urn:ietf:params:netconf:base:1.0</capability>
+ </capabilities>
+</hello>
+""")
+	if ret != 0:
+		print("[FAILED] Sending <hello>")
+		return(-1)
+
+	# receive <hello>
+	result=conn.receive()
+
+        print("#1 - Verify /modules-state/module[name='test-yang-library-submodules'] container is present.")
+
+	get_rpc = """
+<get xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+ <filter type="subtree">
+  <modules-state xmlns="urn:ietf:params:xml:ns:yang:ietf-yang-library">
+   <module><name>test-yang-library-submodules</name></module>
+  </modules-state>
+ </filter>
+</get>
+"""
+	print("get ...")
+	result = conn.rpc(get_rpc)
+
+	print lxml.etree.tostring(result)
+	name = result.xpath('data/modules-state/module/name')
+	assert(len(name)==1)
+
+	namespace = result.xpath('data/modules-state/module/namespace')
+	assert(len(namespace)==1)
+	assert(namespace[0].text=="http://yuma123.org/ns/test-yang-library-submodules")
+
+	conformance_type = result.xpath('data/modules-state/module/conformance-type')
+	assert(len(conformance_type)==1)
+
+	submodule_name = result.xpath('data/modules-state/module/submodule/name')
+	assert(len(submodule_name)==1)
+	assert(submodule_name[0].text=="test-yang-library-submodules-sub1")
+
+        return(0)
+
+sys.exit(main())

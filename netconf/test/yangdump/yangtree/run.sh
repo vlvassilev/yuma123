bd6242ac7a0fcc986a07aef69b7b74ed43a8f588
Vladimir Vassilev <vladimir@transpacket.com>
2018-08-02 16:45:38 +0200

    Modified to validate against /usr/share/yuma/modules

--- netconf/test/yangdump/yangtree/run.sh
+++ netconf/test/yangdump/yangtree/run.sh
@@ -3,45 +3,48 @@
 #./update-module-repository.sh
 
 rm -rf tmp || true
-mkdir tmp
+mkdir -p tmp/pyang
+mkdir -p tmp/yangdump
 
 TOTAL=0
 OKS=0
 FAILS=0
 
-for dst in ietf ; do
-  mkdir -p tmp/pyang/${dst}
-  mkdir -p tmp/yangdump/${dst}
-for module in `ls ../../../modules/${dst}` ; do
+modpath=/usr/share/yuma/modules
+
+for filespec in `find /usr/share/yuma/modules/ -name '*.yang' | sort` ; do
+  module=`basename $filespec .yang`
+  echo $module >&2
   echo $module
-  is_submodule="`head -n 1 ../../../modules/${dst}/${module} | grep submodule`" || true
+
+  is_submodule="`head -n 1 ${filespec} | grep submodule`" || true
   if [ "${is_submodule}" != "" ] ; then
     echo "Skip submodule: ${module}"
     continue
   fi
-  TOTAL=$((${TOTAL}+1))
 
-  pyang_cmd="pyang -f tree --path /usr/share/yuma/modules/:/usr/share/yuma/modules/yang:/usr/share/yuma/modules/ietf:/usr/share/yuma/modules/netconfcentral:/usr/share/yuma/modules/yuma123 ../../../modules/${dst}/${module}"
-  yangdump_cmd="yangdump --format=tree --modpath=/usr/share/yuma/modules/:/usr/share/yuma/modules/yang:/usr/share/yuma/modules/ietf:/usr/share/yuma/modules/netconfcentral:/usr/share/yuma/modules/yuma123 ../../../modules/${dst}/${module}"
+  TOTAL=$((${TOTAL}+1))
+  pyang_cmd="pyang -f tree --path ${modpath} ${filespec}"
+  yangdump_cmd="yangdump --format=tree --modpath=/usr/share/yuma/modules/ ${filespec}"
   echo $pyang_cmd
-  $pyang_cmd | grep -v '^$' | tee > tmp/pyang/${dst}/${module}.tree
+  $pyang_cmd | grep -v '^$' | tee > tmp/pyang/${module}.tree
   pyang_ret=$?
   echo $yangdump_cmd
-  $yangdump_cmd | grep -v '^$' | tee > tmp/yangdump/${dst}/${module}.tree
+  $yangdump_cmd | grep -v '^$' | tee > tmp/yangdump/${module}.tree
   yangdump_ret=$?
-  echo $yangdump_ret
-  echo $pyang_ret
-  cmp tmp/pyang/${dst}/${module}.tree tmp/yangdump/${dst}/${module}.tree
+  echo yangdump_ret=${yangdump_ret} >&2
+  echo pyang_ret=${pyang_ret} >&2
+  cmp tmp/pyang/${module}.tree tmp/yangdump/${module}.tree
   cmp_ret=$?
   if [ "${cmp_ret}" = "0" ] ; then
-    echo OK
+    echo OK: $module
     OKS=$((${OKS}+1))
   else
     FAILS=$((${FAILS}+1))
     echo FAIL: $module
   fi
 done
-done
+
   echo OKS=$OKS
   echo FAILS=$((${TOTAL}-${OKS}))
 exit $FAILS

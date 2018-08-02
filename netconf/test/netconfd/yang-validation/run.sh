e9a3dfc4551e3c10f1d2fc820a0db752fdb41c18
Vladimir Vassilev <vladimir@transpacket.com>
2018-08-02 15:25:47 +0200

    Added support for alternative third party compiler validation: pyang,confd,yanglint

--- netconf/test/netconfd/yang-validation/run.sh
+++ netconf/test/netconfd/yang-validation/run.sh
@@ -2,18 +2,13 @@
 
 #./update-module-repository.sh
 
-if [ "$RUN_WITH_CONFD" != "" ] ; then
-    # skipped test return value
-    exit 77
-fi
-
 rm -rf tmp || true
 mkdir tmp
 
 OKS=0
 FAILS=0
 
-modpath=/usr/share/yuma/modules/
+modpath=/usr/share/yuma/modules
 
 for filespec in `find /usr/share/yuma/modules/ -name '*.yang' | sort` ; do
   module=`basename $filespec`
@@ -24,18 +19,43 @@ for filespec in `find /usr/share/yuma/modules/ -name '*.yang' | sort` ; do
     echo "Skip submodule: ${module}"
     continue
   fi
-  cmd="/usr/sbin/netconfd --validate-config-only --startup-error=stop --module=${filespec} --no-startup --modpath=${modpath}"
-  echo $cmd  >&2
-  $cmd  >&2
-  ret=$?
-  if [ "${ret}" = "0" ] ; then
+
+  if [ "$RUN_WITH_PYANG" != "" ] ; then
+      pyang --path ${modpath}/ ${filespec}  1>&2
+      RES=$?
+  elif [ "$RUN_WITH_YANGDUMP" != "" ] ; then
+      yangdump --modpath=${modpath} ${filespec}  1>&2
+      RES=$?
+  elif [ "$RUN_WITH_YANGLINT" != "" ] ; then
+      yanglint --path=${modpath} ${filespec} 1>&2
+      RES=$?
+  elif [ "$RUN_WITH_CONFD" != "" ] ; then
+      killall -KILL confd || true
+      source $RUN_WITH_CONFD/confdrc
+      for subdir in `find ${modpath} -type d`
+      do
+          confd_modpath=${confd_modpath}${subdir}":"
+      done
+      confd_modpath=${confd_modpath}"."
+
+      cmd="confdc -c ${filespec} --yangpath ${confd_modpath} -o tmp/${module}.fxs"
+      echo $cmd  >&2
+      $cmd  >&2
+      RES=$?
+  else
+      cmd="/usr/sbin/netconfd --validate-config-only --startup-error=stop --module=${filespec} --no-startup --modpath=${modpath}"
+      echo $cmd  >&2
+      $cmd  >&2
+      RES=$?
+  fi
+
+  if [ "${RES}" = "0" ] ; then
     OKS=$((${OKS}+1))
     echo OK: $module
   else
     FAILS=$((${FAILS}+1))
     echo FAIL: $module
   fi
-done
 done
   echo OKS=$OKS
   echo FAILS=$FAILS

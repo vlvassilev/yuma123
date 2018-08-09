1f6e1fe5aaed7868d732416f02f80cc62ca4d2f8
Vladimir Vassilev <vladimir@transpacket.com>
2018-08-09 13:11:21 +0200

    Updated testcase to test the rfc8344 modules and examples.

--- netconf/test/netconfd/ietf-ip-bis/run.sh
+++ netconf/test/netconfd/ietf-ip-bis/run.sh
@@ -3,38 +3,38 @@
 rm -rf tmp || true
 mkdir tmp
 cd tmp
-wget https://tools.ietf.org/id/draft-ietf-netmod-rfc7223bis-00.txt
-wget https://tools.ietf.org/id/draft-ietf-netmod-rfc7277bis-00.txt
-rfcstrip draft-ietf-netmod-rfc7223bis-00.txt
-rfcstrip draft-ietf-netmod-rfc7277bis-00.txt
-pyang --ietf -f tree --path ./:../../../../modules/ietf-draft/:../../../../modules/ietf/ ietf-interfaces@2017-08-17.yang
-pyang -f tree --path ./:../../../../modules/ietf-draft/:../../../../modules/ietf/ ex-ethernet-bonding.yang
-pyang -f tree --path ./:../../../../modules/ietf-draft/:../../../../modules/ietf/ ex-ethernet.yang
-pyang -f tree --path ./:../../../../modules/ietf-draft/:../../../../modules/ietf/ ex-vlan.yang
-pyang --ietf -f tree --path ./:../../../../modules/ietf-draft/:../../../../modules/ietf/ ietf-ip@2017-08-21.yang
+wget http://www.yang-central.org/twiki/pub/Main/YangTools/rfcstrip
+
+wget https://www.rfc-editor.org/rfc/rfc8343.txt
+wget https://www.rfc-editor.org/rfc/rfc8344.txt
+sh ./rfcstrip rfc8343.txt
+sh ./rfcstrip rfc8344.txt
+pyang --ietf -f tree --path ./:../../../../modules/ietf-draft/:../../../../modules/ietf/ ietf-interfaces@2018-02-20.yang
+pyang -f tree --path ./:../../../../modules/ietf-draft/:../../../../modules/ietf/ example-ethernet-bonding.yang
+pyang -f tree --path ./:../../../../modules/ietf-draft/:../../../../modules/ietf/ example-ethernet.yang
+pyang -f tree --path ./:../../../../modules/ietf-draft/:../../../../modules/ietf/ example-vlan.yang
+pyang --ietf -f tree --path ./:../../../../modules/ietf-draft/:../../../../modules/ietf/ ietf-ip@2018-02-22.yang
 
 if [ "$RUN_WITH_CONFD" != "" ] ; then
   killall -KILL confd || true
   echo "Starting confd: $RUN_WITH_CONFD"
   source $RUN_WITH_CONFD/confdrc
-  cd tmp
-  for module in ietf-interfaces@2017-08-17 iana-if-type ietf-ip ex-ethernet-bonding.yang ex-ethernet.yang ex-vlan.yang ; do
-    cp ../../../../modules/ietf/${module}.yang .
-    confdc -c ${module}.yang --yangpath ../../../../
+  for module in ietf-interfaces@2018-02-20 ietf-ip@2018-02-22 example-ethernet-bonding example-ethernet example-vlan ../../../../modules/ietf/iana-if-type@2014-05-08; do
+    confdc -c ${module}.yang --yangpath .:../../../../modules/ietf
   done
   NCPORT=2022
   NCUSER=admin
   NCPASSWORD=admin
-  confd --verbose --foreground --addloadpath ${RUN_WITH_CONFD}/src/confd --addloadpath ${RUN_WITH_CONFD}/src/confd/yang --addloadpath ${RUN_WITH_CONFD}/src/confd/aaa --addloadpath ${RUN_WITH_CONFD}/etc/confd --addloadpath .
+  confd --verbose --foreground --addloadpath ${RUN_WITH_CONFD}/src/confd --addloadpath ${RUN_WITH_CONFD}/src/confd/yang --addloadpath ${RUN_WITH_CONFD}/src/confd/aaa --addloadpath ${RUN_WITH_CONFD}/etc/confd --addloadpath . 2>&1 1>server.log &
   SERVER_PID=$!
   cd ..
 else
   killall -KILL netconfd || true
   rm /tmp/ncxserver.sock || true
 
-  /usr/sbin/netconfd --modpath=.:/usr/share/yuma/modules --module=iana-if-type --module=./ietf-interfaces@2017-08-17.yang --module=./ietf-ip@2017-08-21.yang --module=ex-ethernet-bonding.yang  --module=ex-ethernet.yang  --module=ex-vlan.yang --no-startup --validate-config-only --superuser=$USER
-  /usr/sbin/netconfd --modpath=.:/usr/share/yuma/modules --module=iana-if-type --module=./ietf-interfaces@2017-08-17.yang --module=./ietf-ip@2017-08-21.yang --module=ex-ethernet-bonding.yang  --module=ex-ethernet.yang  --module=ex-vlan.yang --module=test-ietf-ip-bis --no-startup --superuser=$USER 2>&1 1>server.log &
-   SERVER_PID=$!
+  /usr/sbin/netconfd --modpath=.:/usr/share/yuma/modules --module=iana-if-type --module=./ietf-interfaces@2018-02-20.yang --module=./ietf-ip@2018-02-22.yang --module=example-ethernet-bonding.yang  --module=example-ethernet.yang  --module=example-vlan.yang --no-startup --validate-config-only --superuser=$USER
+  /usr/sbin/netconfd --with-nmda=true --modpath=.:/usr/share/yuma/modules --module=iana-if-type --module=./ietf-interfaces@2018-02-20.yang --module=./ietf-ip@2018-02-22.yang --module=example-ethernet-bonding.yang  --module=example-ethernet.yang  --module=example-vlan.yang --module=test-ietf-ip-bis --no-startup --superuser=$USER 2>&1 1>server.log &
+  SERVER_PID=$!
   cd ..
 fi
 

vlvassilev/yuma123

commit 61fe02f9b19007fd043b01512916d06b5234eeb7

fix include statements for some libxml files
The libxml header files are installed in the subdirectory libxml2/libxml.
Prepend "libxml/" to the included filenames so that -I/usr/include/libxml2
is sufficient to find the headers.

netconf/src/ydump/c.c
@@ -33,8 +33,8 @@ date         init     comment
 #include <memory.h>
 #include <ctype.h>
 
-#include <xmlstring.h>
-#include <xmlreader.h>
+#include <libxml/xmlstring.h>
+#include <libxml/xmlreader.h>
 
 #include "procdefs.h"
 #include "c.h"

netconf/src/ydump/h.c
@@ -52,8 +52,8 @@ date         init     comment
 #include <memory.h>
 #include <ctype.h>
 
-#include <xmlstring.h>
-#include <xmlreader.h>
+#include <libxml/xmlstring.h>
+#include <libxml/xmlreader.h>
 
 #include "procdefs.h"
 #include "c.h"

commit 9f59bfff1d56125a119bbf366e76e61cbf0006d2

yangdump: update generated xmlstring header include path
This change was missing from the commit that adjusted libxml include
statements.

Fixes: 61fe02f9b190 ("fix include statements for some libxml files")

netconf/src/ydump/c.c
@@ -415,7 +415,7 @@ static void
     neednotif = need_notif_includes(mod, cp);
 
     /* add xmlChar include */
-    write_ext_include(scb, (const xmlChar *)"xmlstring.h");
+    write_ext_include(scb, (const xmlChar *)"libxml/xmlstring.h");
 
     /* add procdefs include first NCX include */
     write_ncx_include(scb, (const xmlChar *)"procdefs");

netconf/src/ydump/h.c
@@ -563,7 +563,7 @@ static void
     neednotif = need_notif_includes(mod, cp);
 
     /* add xmlChar include */
-    write_ext_include(scb, (const xmlChar *)"xmlstring.h");
+    write_ext_include(scb, (const xmlChar *)"libxml/xmlstring.h");
 
     if (cp->isuser) {
         write_ncx_include(scb, (const xmlChar *)"agt");
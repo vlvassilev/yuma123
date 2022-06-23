363b6e60e15ae85e178b1c4eeb2e4ddbf48405c6
ian <ian@intrising.com.tw>
2022-06-23 16:39:55 +0800

    feat: move into tests

---
+++ libintri/tests/src/intri-core/intri-vlan/intri-vlan-trans.c
@@ -0,0 +1,7 @@
+
+
+#include "intri-vlan-trans.h"
+
+void ian_test_func() {
+  printf("ian test func ok");
+}
---
+++ libintri/tests/src/intri-core/intri-vlan/intri-vlan-trans.h
@@ -0,0 +1,11 @@
+
+
+#ifndef _H_intri_vlan_trans
+#define _H_intri_vlan_trans
+
+#include <stdio.h>
+#include <stdlib.h>
+
+extern void ian_test_func();
+
+#endif

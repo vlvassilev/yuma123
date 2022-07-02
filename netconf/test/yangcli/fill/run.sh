a5cb5ef573856a1cec315aebb9a56bd93c4f1da9
Vladimir Vassilev <vladimir@lightside-instruments.com>
2022-07-02 21:47:24 +0200

    Added testcase for validating the 'fill' command used for rpc input entry

---
+++ netconf/test/yangcli/fill/run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+rm -rf tmp || true
+mkdir tmp
+if [ "$RUN_WITH_CONFD" != "" ] ; then
+  cd tmp
+  killall -KILL confd || true
+  echo "Starting confd: $RUN_WITH_CONFD"
+  source $RUN_WITH_CONFD/confdrc
+  confdc -c mod1.yang --yangpath -o mod1.fxs
+  export NCSERVER=localhost
+  export NCPORT=2022
+  export NCUSER=admin
+  export NCPASSWORD=admin
+  confd --verbose --foreground --addloadpath ${RUN_WITH_CONFD}/src/confd --addloadpath ${RUN_WITH_CONFD}/src/confd --addloadpath ${RUN_WITH_CONFD}/src/confd/yang --addloadpath ${RUN_WITH_CONFD}/src/confd/aaa --addloadpath . 2>&1 1>server.log &
+  SERVER_PID=$!
+  cd ..
+else
+  killall -KILL netconfd || true
+  export NCSERVER=localhost
+  export NCPORT=830
+  export NCUSER=${USER}
+  export NCPASSWORD=""
+  rm /tmp/ncxserver.sock || true
+  /usr/sbin/netconfd --module=mod1.yang --no-startup --superuser=$USER 2>&1 1>tmp/server.log &
+  SERVER_PID=$!
+fi
+sleep 3
+expect session.exp
+kill -KILL $SERVER_PID
+cat tmp/server.log
+sleep 1

036dcd3d9cba440389c85432ee872a1022d2df8e
Vladimir Vassilev <vladimir@lightside-instruments.com>
2022-12-05 16:58:05 +0100

    Bumped up release version for new 2.14 release cycle.

--- netconf/src/ncx/version.h
+++ netconf/src/ncx/version.h
@@ -53,7 +53,7 @@ extern "C" {
 *                                                                   *
 *********************************************************************/
 
-#define YUMA_VERSION  (const xmlChar *)"2.13"
+#define YUMA_VERSION  (const xmlChar *)"2.14"
 
 #ifdef __cplusplus
 }  /* end extern 'C' */

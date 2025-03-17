86b0f83ee2474ed270cbe102b815c53ec132af4f
Vladimir Vassilev <vladimir@lightside-instruments.com>
2025-03-17 01:07:55 +0100

    Release 2.14-alpha1

--- netconf/python/setup.py
+++ netconf/python/setup.py
@@ -1,4 +1,9 @@
-from distutils.core import setup, Extension
+import sys
+
+if sys.version_info < (3,10):
+    from distutils.core import setup,Extension
+else:
+    from setuptools import setup,Extension
 
 yuma = Extension('yuma',
                    sources = ['yuma.c'],

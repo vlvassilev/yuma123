eee8d92eca3b736df0a2a9f24c759658faf5a932
Vladimir Vassilev <vladimir@lightside-instruments.com>
2025-05-20 14:58:08 +0200

    Added getopt based parameter parsing to yangrpc-parse-example

--- netconf/src/yangrpc/example/yangrpc-parse-example.c
+++ netconf/src/yangrpc/example/yangrpc-parse-example.c
@@ -1,12 +1,35 @@
+/* Usage:
+   ./yangrpc-parse-example --server=myserver.com --port=830 --user=vladimir --password='mypass' \
+                     --private-key=/home/vladimir/.ssh/id_rsa \
+                     --public-key=/home/vladimir/.ssh/id_rsa.pub
+*/
+
 #include <assert.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <getopt.h>
 
 #include "ncx.h"
+#include "ncxmod.h"
 #include "val.h"
 #include "yangrpc.h"
 
+static struct option const long_options[] =
+{
+    {"autoload", optional_argument, NULL, 'a'},
+    {"server", required_argument, NULL, 's'},
+    {"port", required_argument, NULL, 'p'},
+    {"user", required_argument, NULL, 'u'},
+    {"password", required_argument, NULL, 'P'},
+    {"private-key", required_argument, NULL, 'k'},
+    {"public-key", required_argument, NULL, 'K'},
+    {NULL, 0, NULL, 0}
+};
+
 int main(int argc, char* argv[])
 {
+    int i;
     status_t res;
     yangrpc_cb_ptr_t yangrpc_cb_ptr;
     ncx_module_t * ietf_netconf_mod;
@@ -20,31 +43,86 @@ int main(int argc, char* argv[])
     val_value_t* type_meta_val;
     val_value_t* select_meta_val;
 
+    int optc;
+
+    char* server="127.0.0.1";
+    unsigned int port=830;
+    char* user=getlogin();
+    char* password=NULL;
+    char private_key[1024];
+    char public_key[1024];
+    boolean autoload = TRUE;
+
+    sprintf(private_key,"/home/%s/.ssh/id_rsa",user);
+    sprintf(public_key,"/home/%s/.ssh/id_rsa.pub",user);
+
+    while ((optc = getopt_long (argc, argv, "a:s:p:u:P:k:K", long_options, NULL)) != -1) {
+        switch (optc) {
+            case 'a':
+                if (optarg != NULL && strcasecmp(optarg, "false") == 0)
+                    autoload = FALSE;
+                break;
+            case 's':
+                server=optarg;
+                break;
+            case 'p':
+                port = atoi(optarg);
+                break;
+            case 'u':
+                user = optarg;
+                break;
+            case 'P':
+                password = optarg;
+                break;
+            case 'k':
+                strcpy(private_key,optarg);
+                break;
+            case 'K':
+                strcpy(public_key,optarg);
+                break;
+            default:
+                exit (-1);
+        }
+    }
+
+
     res = yangrpc_init(NULL);
     assert(res==NO_ERR);
-    res = yangrpc_connect("localhost"/*server*/, 830/*port*/, "vladimir"/*user*/,"mysecretpass"/*password*/,"/home/vladimir/.ssh/id_rsa.pub"/*public_key*/, "/home/vladimir/.ssh/id_rsa"/*private_key*/, NULL, &yangrpc_cb_ptr);
+
+    res = yangrpc_connect(server /*127.0.0.1*/,
+                          port /*830*/,
+                          user /*vladimir*/,
+                          password /* "" */,
+                          public_key /* "/home/vladimir/.ssh/id_rsa.pub" */,
+                          private_key /* "/home/vladimir/.ssh/id_rsa" */,
+                          autoload ? "--autoload=true" : "--autoload=false",
+                          &yangrpc_cb_ptr);
     assert(res==NO_ERR);
 
-    res = yangrpc_parse_cli(yangrpc_cb_ptr, "xget /interfaces-state", &request_val);
+    res = yangrpc_parse_cli(yangrpc_cb_ptr, "xget /", &request_val);
     assert(res==0);
 
-    res = yangrpc_exec(yangrpc_cb_ptr, request_val, &reply_val);
-    assert(res==0);
+    for(i=0;i<100;i++) {
+        printf("i=%d\n",i);
 
-    val_dump_value(reply_val,0);
-    {
-    	val_value_t* data_val;
-    	val_value_t* interfaces_state_val;
-        data_val = val_find_child(reply_val,NULL,"data");
-        interfaces_state_val = val_find_child(data_val,"ietf-interfaces","interfaces-state");
-        val_dump_value(interfaces_state_val,0);
+        res = yangrpc_exec(yangrpc_cb_ptr, request_val, &reply_val);
+        assert(res==0);
+        val_dump_value(reply_val,0);
+        if(0){
+            val_value_t* data_val;
+            val_value_t* interfaces_state_val;
+            data_val = val_find_child(reply_val,NULL,"data");
+            interfaces_state_val = val_find_child(data_val,"ietf-interfaces","interfaces-state");
+            val_dump_value(interfaces_state_val,0);
+            res = yangrpc_exec(yangrpc_cb_ptr, request_val, &reply_val);
+            assert(res==0);
+        }
+ 
+        val_free_value(reply_val);
     }
 
     val_free_value(request_val);
-    val_free_value(reply_val);
-
     yangrpc_close(yangrpc_cb_ptr);
 
     return 0;
 }
-

548be643a3cd606779cef906ed883a992ae6dc3e
Vladimir Vassilev <vladimir@lightside-instruments.com>
2025-05-24 19:32:28 +0200

    Added testcase for yangrpc multi session support validation

--- netconf/test/yangrpc/get/run.sh
+++ netconf/test/yangrpc/get/run.sh
@@ -7,15 +7,20 @@ NCPASSWORD=""
 
 killall -KILL netconfd || true
 rm -f /tmp/ncxserver.sock
+rm -rf tmp || true
+mkdir tmp
+
 /usr/sbin/netconfd --no-startup --log-level=debug3 --target=running --superuser=$USER &
 SERVER_PID=$!
 
 sleep 4
-valgrind -v --log-fd=1 --num-callers=100 --leak-check=full --show-leak-kinds=all  -- ./yangrpc-example --server=$NCSERVER --port=$NCPORT --user=$NCUSER --password=$NCPASSWORD 1>session.stdout 2>session.stderr
+
+valgrind -v --log-fd=1 --num-callers=100 --leak-check=full --show-leak-kinds=all  -- ./yangrpc-example --server=$NCSERVER --port=$NCPORT --user=$NCUSER --password=$NCPASSWORD 1>tmp/session.stdout 2>tmp/session.stderr
+
 
 for var in "$@"
 do
     if [ "$var" == "--check-memory-leak" ] ; then
-        cat tmp/server.log | grep "ERROR SUMMARY: 0 errors"        
+        cat tmp/session.stdout | grep "ERROR SUMMARY: 0 errors"
     fi
 done
---
+++ netconf/test/yangrpc/multi-session/run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+NCSERVER=localhost
+NCPORT=830
+NCUSER=$USER
+NCPASSWORD=""
+
+killall -KILL netconfd || true
+rm -f /tmp/ncxserver*.sock
+rm -rf tmp || true
+mkdir tmp
+
+/usr/sbin/netconfd --no-startup --log-level=debug3 --target=running --superuser=$USER &
+SERVER_PID=$!
+
+/usr/sbin/netconfd --no-startup --log-level=debug3 --target=running --superuser=$USER --port=10830 --ncxserver-sockname=/tmp/ncxserver-10830.sock &
+SERVER_PID_ALT=$!
+
+sleep 4
+
+valgrind -v --log-fd=1 --num-callers=100 --leak-check=full --show-leak-kinds=all  -- ./yangrpc-multi-session-example --server=$NCSERVER --port=$NCPORT --server=$NCSERVER --port=10830 --user=$NCUSER --password=$NCPASSWORD 1>tmp/session.stdout 2>tmp/session.stderr
+
+
+for var in "$@"
+do
+    if [ "$var" == "--check-memory-leak" ] ; then
+         cat tmp/session.stdout | grep "ERROR SUMMARY: 0 errors"
+    fi
+done
+
+kill -KILL $SERVER_PID
+kill -KILL $SERVER_PID_ALT
